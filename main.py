b12473d39e9220a42e31623b719b575e3041ee27
nikuela <nicolaspenso@gmail.com>
2025-06-23 18:19:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-
 from kivy.app import App
 from kivy.uix.label import Label
 
-class AviatorApp(App):
+class MiApp(App):
     def build(self):
-        return Label(text='Aviator Predictor')
+        return Label(text="¡Mi APK se generó con éxito!", font_size=24)
 
-if __name__ == '__main__':
-    AviatorApp().run()
+if __name__ == "__main__":
+    MiApp().run()
